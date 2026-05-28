b95d1ac94e4e0a03e7924ec313168ab33d9a19f0
Vinayyy21_ <vinaykairamkonda19110@gmail.com>
2026-05-28 12:27:25 +0530

    Completed Week 3 Day 5 FastAPI prediction

--- API/app.py
+++ API/app.py
@@ -1,10 +1,42 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+import pandas as pd
+
+import joblib
+
 app = FastAPI()
+
+model = joblib.load("../Models/rf_ltv_model.pkl")
+
+class CustomerData(BaseModel):
+
+    tenure: int
+
+    MonthlyCharges: float
 
 @app.get("/")
 def home():
 
     return {
-        "message": "Customer Churn & LTV API Running"
+
+        "message": "LTV Prediction API Running"
+    }
+
+@app.post("/predict")
+def predict(data: CustomerData):
+
+    input_data = pd.DataFrame([{
+
+        'tenure': data.tenure,
+
+        'MonthlyCharges': data.MonthlyCharges
+    }])
+
+    prediction = model.predict(input_data)
+
+    return {
+
+        "PredictedLTV": float(prediction[0])
     }
