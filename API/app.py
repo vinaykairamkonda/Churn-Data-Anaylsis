afd49828c4e3a8377c5dc6f04deba2f0e9d6bbf0
Vinayyy21_ <vinaykairamkonda19110@gmail.com>
2026-05-28 14:08:43 +0530

    Added batch prediction API endpoint

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 from pydantic import BaseModel
 
@@ -6,25 +6,40 @@ import pandas as pd
 
 import joblib
 
+import io
+
+
+# Create FastAPI app
 app = FastAPI()
 
+
+# Load trained model
 model = joblib.load("../Models/rf_ltv_model.pkl")
 
+
+# Input schema
 class CustomerData(BaseModel):
 
     tenure: int
 
     MonthlyCharges: float
 
+
+# Home route
 @app.get("/")
+
 def home():
 
     return {
 
         "message": "LTV Prediction API Running"
+
     }
 
+
+# Single customer prediction
 @app.post("/predict")
+
 def predict(data: CustomerData):
 
     input_data = pd.DataFrame([{
@@ -32,6 +47,7 @@ def predict(data: CustomerData):
         'tenure': data.tenure,
 
         'MonthlyCharges': data.MonthlyCharges
+
     }])
 
     prediction = model.predict(input_data)
@@ -39,4 +55,33 @@ def predict(data: CustomerData):
     return {
 
         "PredictedLTV": float(prediction[0])
+
     }
+
+
+# Batch prediction endpoint
+@app.post("/batch_predict")
+
+async def batch_predict(
+
+    file: UploadFile = File(...)
+):
+
+    contents = await file.read()
+
+    df = pd.read_csv(
+
+        io.StringIO(
+
+            contents.decode('utf-8')
+        )
+    )
+
+    predictions = model.predict(df)
+
+    df['PredictedLTV'] = predictions
+
+    return df.to_dict(
+
+        orient='records'
+    )
